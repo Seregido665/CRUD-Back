2ef4f567aa5dc776c5224b9eb18dc18bce416796
Cdelatorre <carlosdelatorrediaz@gmail.com>
2025-12-09 09:42:04 +0100

    add models

--- src/app.js
+++ src/app.js
@@ -1,25 +1,41 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const express = require("express");
 const path = require("path");
+const BookModel = require("./models/Book.model");
+const UserModel = require("./models/User.model");
 const app = express();
 
 const PORT = process.env.PORT;
 const dataBooks = require(path.join(__dirname, "data.json"));
 const dataLibraries = require(path.join(__dirname, "libraries.json"));
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Conectado a MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error al conectar a MongoDB:", error.message);
+  });
+
 app.use(express.json());
+
+// BOOKS
 
 app.get("/", (req, res, next) => {
   res.send("Hello, this is the Book API!");
 });
 
 app.get("/books", (req, res, next) => {
-  res.json(dataBooks);
-});
-
-app.get("/libraries", (req, res, next) => {
-  res.json(dataLibraries);
+  BookModel.find()
+    .then((books) => {
+      res.json(books);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 app.get("/books/:id", (req, res, next) => {
@@ -30,6 +46,36 @@ app.get("/books/:id", (req, res, next) => {
   });
 
   res.json(book);
+});
+
+app.post("/books", (req, res, next) => {
+  const newBook = req.body;
+
+  BookModel.create(newBook)
+    .then((bookCreated) => {
+      res.json("Book created successfully!!");
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// USERS
+app.post("/users", (req, res, next) => {
+  const newUser = req.body;
+
+  UserModel.create(newUser)
+    .then((user) => {
+      res.json("Usuario creado correctamente");
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// LIBRARIES
+app.get("/libraries", (req, res, next) => {
+  res.json(dataLibraries);
 });
 
 app.listen(PORT, () => {
---
+++ src/models/User.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: [8, "Almenos 8 caracteres!"],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
