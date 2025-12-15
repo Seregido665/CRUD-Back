485240100a9bf728fcc2518f692c131b08df181e
Sergio <sergiotestera666@gmail.com>
2025-12-15 13:09:53 +0100

    Mejora del codigo y mas consultas a la base de datos

--- src/models/Book.model.js
+++ src/models/Book.model.js
@@ -1,17 +1,64 @@
+/*const mongoose = require("mongoose"); 
+
+const bookSchema = new mongoose.Schema({ 
+  title: { 
+    type: String, 
+    required: true, 
+  }, 
+  author: { 
+    type: String, 
+    required: true, 
+  }, 
+  year: { 
+    type: Number, 
+  }, 
+              // --- PARA React ---
+  user: {     // CON ESTO GUARDAMOS EL id DEL user QUE CREO EL LIBRO
+              // PARA LUEGO USAR EL .populate
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+},
+  {
+    // MONGO GENERA UN ID PERO ASI: _id
+    // CON ESTE .json
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id; // Pasamos '_id' a 'id'
+        delete ret._id;   // Eliminamos '_id'
+      },
+    },
+  }
+);
+    
+module.exports = mongoose.model("Book", bookSchema);
+*/
+
 const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+  title: { 
+    type: String, 
+    required: true 
   },
-  author: {
-    type: String,
-    required: true,
+  author: { 
+    type: String, 
+    required: true 
   },
-  year: {
-    type: Number,
-  },
+  year: Number,
+  user: { 
+    type: String, 
+    required: true 
+  }
+}, {
+  // MONGO GENERA UN ID PERO ASI: _id
+  // CON ESTE .json
+  toJSON: {
+    transform: (doc, ret) => {
+      ret.id = ret._id; // Pasamos '_id' a 'id'
+      delete ret._id;   // Eliminamos '_id'
+    }
+  }
 });
 
 module.exports = mongoose.model("Book", bookSchema);
--- src/models/User.model.js
+++ src/models/User.model.js
@@ -12,9 +12,34 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "La contraseña es requerida!"],
     minLength: [8, "Almenos 8 caracteres!"],
   },
 });
+
+
+/*
+// --- HASHEO DE contraseñas --> SOLO PARA React ---
+// .pre PARA INDICAR QUE SE HACE ANTES DE save
+userSchema.pre("save", function (next) {   
+  const user = this;
+
+  // PARA COMPROBAR QUE NO SE HA CAMBIADO LA CONTRASEÑA
+  // Y QUE NO SE hashee DE NUEVO.
+  if (!user.isModified("password")) {  
+    return next();
+  }
+
+  // VALOR ALEATORIO UNICO A MI CONTRASEÑA
+  // CON 10 RONDAS DE ENCRIPTACION.
+  const salt = bcrypt.genSaltSync(10);
+
+  // ENCRIPTA LA CONTRASEÑA
+  const hashedPassword = bcrypt.hashSync(user.password, salt);
+  
+  // Y CAMBIAMOS NUESTRA CONTRASEÑA POR EL hasheo RESULTANTE
+  user.password = hashedPassword;
+});
+*/
 
 module.exports = mongoose.model("User", userSchema);
